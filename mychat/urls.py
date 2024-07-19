235d62e001a0ef49cbd7d4b8adcd7c56771eca8a
Your Name <you@example.com>
2024-07-18 22:45:12 -0700

    add gpt-4o

--- mychat/urls.py
+++ mychat/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="my-index")
+    path("", views.index, name="my-index"),
+    path("gpt3", views.gpt3),
 ]
--- mychat/views.py
+++ mychat/views.py
@@ -4,28 +4,61 @@ from openai import OpenAI
 import os
 
 # Create your views here.
-def index(request):
+def gpt3(request):
+    print("gpt3")
     if request.method == "POST":
         form = ChatForm(request.POST)
         
+        
         if form.is_valid():
-            # get question user asked
-            question = form.cleaned_data['question']
-            
             # set up an open api client
             client = OpenAI(api_key=os.getenv("api_key"))
             
             # use open api completion api to get ans
             res = client.completions.create(
                 model="gpt-3.5-turbo-instruct",
-                prompt=question,
+                prompt=form.cleaned_data['question'],
                 max_tokens=300,
                 temperature=0,
             )
             
             return render(request, "mychat/answer.html", {
-                "question" : question,
-                "answer": res.choices[0].text
+                "question" : form.cleaned_data['question'],
+                "answer": res.choices[0].text,
+                "uri": "/mychat/gpt3",
+            })
+        else:
+            print("not valid send me haha")
+    else:
+        form = ChatForm()
+        return render(request, "mychat/index.html", {
+            "form": form
+        })
+    
+
+# Create your views here.
+def index(request):
+    print("gpt4")
+    if request.method == "POST":
+        form = ChatForm(request.POST)
+        
+        if form.is_valid():
+            # set up an open api client
+            client = OpenAI(api_key=os.getenv("api_key"))
+          
+            completion = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": form.cleaned_data['question']}
+                    ],
+                n=1,
+            )
+
+            return render(request, "mychat/answer.html", {
+                "question" : form.cleaned_data['question'],
+                "answer": completion.choices[0].message.content,
+                "uri": "/mychat/",
             })
         else:
             print("not valid send me haha")
