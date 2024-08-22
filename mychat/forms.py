b8c0192d8e6f5a3b93de8fc457b4117435663ac5
Leo Shen <you@example.com>
2024-08-22 09:54:12 -0700

    add autofocus for textarea

--- mychat/forms.py
+++ mychat/forms.py
@@ -2,6 +2,10 @@ from django import forms
 
 class ChatForm(forms.Form):
     question = forms.CharField(
-        widget=forms.Textarea(attrs={"rows":"4", "placeholder": "your question"}),
+        widget=forms.Textarea(attrs={
+        "rows":"4", 
+        "placeholder": "your question",
+        "autofocus":True
+        }),
         label= "Ask AI a question"
     )
