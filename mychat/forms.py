8962430d2645488fbb0b930a3b83cd1b46122668
Leo Shen <you@example.com>
2024-09-05 06:42:23 -0700

    add more rows for input form

--- mychat/forms.py
+++ mychat/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class ChatForm(forms.Form):
     question = forms.CharField(
         widget=forms.Textarea(attrs={
-        "rows":"4", 
+        "rows":"10", 
         "placeholder": "your question",
         "autofocus":True
         }),
